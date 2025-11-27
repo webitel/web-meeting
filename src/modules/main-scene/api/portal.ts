bd785fa436b70cc987c9302bd4dbe23d461ec383
Daniil Lohvinov <nsblnr@gmail.com>
2025-11-27 18:48:49 +0200

    refactor: update postPortalToken function to use TypeScript types for credentials and return value [WTEL-7680](https://webitel.atlassian.net/browse/WTEL-7680)

--- src/modules/main-scene/api/portal.ts
+++ src/modules/main-scene/api/portal.ts
@@ -1,9 +1,10 @@
-import { instance } from '../../../app/api/instance'
+import { instance } from '../../../app/api/instance';
+import { TokenRequest, AccessToken } from '@buf/webitel_portal.community_timostamm-protobuf-ts/data/auth_pb';
 
-const postPortalToken = async (credentials) => {
+const postPortalToken = async (credentials: TokenRequest): Promise<AccessToken> => {
 
   const url = '/portal/token';
-  return await instance.post(url, credentials)
+  return instance.post(url, credentials)
 };
 
 export const PortalAPI = {
