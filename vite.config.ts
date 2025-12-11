e343687394813ac4b3fab8a51ddd31a357989543
Lera24 <bedulinavalera21@gmail.com>
2025-12-11 16:41:58 +0200

    fix: vite.config [WTEL-7680](https://webitel.atlassian.net/browse/WTEL-7680)

--- vite.config.ts
+++ vite.config.ts
@@ -1,55 +1,48 @@
 import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
-export default ({ mode }) => {
-	const env = loadEnv(mode, process.cwd(), '');
-
-	return defineConfig({
-		base: '/meet',
-		define: {
-			'process.env': JSON.stringify(env),
+export default defineConfig({
+	base: '/meet',
+	build: {
+		chunkSizeWarningLimit: 20000,
+		// rolldownOptions: {
+		//   external: ['webitel-sdk'],
+		// },
+	},
+	plugins: [
+		vue(),
+		vueDevTools({
+			launchEditor: 'cursor',
+		}),
+	],
+	server: {
+		port: 8080,
+		// https: true,
+	},
+	resolve: {
+		alias: {
+			'@aliasedDeps/api-services/axios': resolve(
+				__dirname,
+				'src/app/api/instance',
+			),
+			lodash: 'lodash-es',
 		},
-		build: {
-			chunkSizeWarningLimit: 20000,
-			// rolldownOptions: {
-			//   external: ['webitel-sdk'],
-			// },
-		},
-		plugins: [
-			vue(),
-			vueDevTools({
-				launchEditor: 'cursor',
-			}),
+		dedupe: [
+			'vue',
 		],
-		server: {
-			port: 8080,
-			// https: true,
-		},
-		resolve: {
-			alias: {
-				'@aliasedDeps/api-services/axios': resolve(
-					__dirname,
-					'src/app/api/instance',
-				),
-				lodash: 'lodash-es',
-			},
-			dedupe: [
-				'vue',
-			],
-		},
-		// fixes "has no export named "default" (or not default)
-		optimizeDeps: {
-			include: [
-				'ee-ts',
-				'deepmerge',
-				'deep-copy',
-				'deep-equal',
-				'clipboard-copy',
-				'webitel-sdk',
-			],
-		},
-	});
-};
+	},
+	// fixes "has no export named "default" (or not default)
+	optimizeDeps: {
+		include: [
+			'ee-ts',
+			'deepmerge',
+			'deep-copy',
+			'deep-equal',
+			'clipboard-copy',
+			'webitel-sdk',
+		],
+	},
+});
