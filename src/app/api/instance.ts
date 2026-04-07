5f9e21356a2646c848009b5e0d7dddcb64380d49
Олександр Пальонний <s2567k@gmail.com>
2026-04-07 23:26:09 +0300

    fix: configure api instance with custom base url [WTEL-9274](https://webitel.atlassian.net/browse/WTEL-9274)

--- src/app/api/instance.ts
+++ src/app/api/instance.ts
@@ -1,5 +1,11 @@
-import { getDefaultInstance } from '@webitel/api-services/api/defaults';
+import { generateInstance } from '@webitel/api-services/api/axios';
 
-export const instance = getDefaultInstance();
+export const instance = generateInstance({
+	baseURL: import.meta.env.VITE_API_URL,
+});
+
+delete (instance.defaults.headers as Record<string, unknown>)[
+	'X-Webitel-Access'
+];
 
 export default instance;
